8737edab1974f6848d8c33fa328903f41df3e375
lokada <lyokada@ucsc.edu>
2014-09-16 14:52:02 -0700

    initial commit

---
+++ CandyTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CandyTutorial
+//
+//  Created by Lynne Okada on 9/16/14.
+//  Copyright (c) 2014 Lynne Okada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CandyTutorial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CandyTutorial
+//
+//  Created by Lynne Okada on 9/16/14.
+//  Copyright (c) 2014 Lynne Okada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
