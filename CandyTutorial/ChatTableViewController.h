49373f0a225e08aebf620688d18f71de7feb131d
lokada <lyokada@ucsc.edu>
2014-09-24 00:37:59 -0700

    chat started

---
+++ CandyTutorial/ChatTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatTableViewController.h
+//  CandyTutorial
+//
+//  Created by Lynne Okada on 9/23/14.
+//  Copyright (c) 2014 Lynne Okada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatTableViewController : UITableViewController
+
+@end
